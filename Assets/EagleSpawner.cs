f83965a56311d3212f2b8a21b2538b8c131cef02
Saoirse <lynchsaoirse@gmail.com>
2018-03-13 09:44:02 +0000

    Initial version

---
+++ Assets/EagleSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleSpawner : MonoBehaviour {
+
+	public float gap = 20;
+	public float Followers = 2;
+
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
